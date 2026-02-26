46b1bc4d952384615fbfe9e3e7edcbe8dfe24737
logesvenugopal <logesvenugopal@gmail.com>
2026-02-26 16:40:37 +0530

    DataTypesEx

---
+++ DataTypesExercise2.java
@@ -0,0 +1,29 @@
+class DataTypesExercise2
+{
+	public static void main(String[] args)
+	{
+		byte age = 23;
+		System.out.println(age);
+		
+		short total=455;
+		System.out.println(total);
+		
+		int total1=10000;
+		System.out.println(total1);
+		
+		long number=8608709544l;
+		System.out.println(number);
+		
+		float percentage=56.6f;
+		System.out.println(percentage);
+		
+		double salary = 450000.50;
+		System.out.println(salary);
+		
+		char initials = 'M';
+		System.out.println(initials);
+		
+		boolean yesOrNo = true;
+		System.out.println(yesOrNo);
+	}
+}
